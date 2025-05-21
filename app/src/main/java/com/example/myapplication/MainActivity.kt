685f11c258d13a8557b493b87aec510b6396f87d
Megha Tavrech <megha.tavrech@galaxyweblinks.in>
2025-05-21 11:59:55 +0530

    CIRunTest

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -32,7 +32,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    if (name.isNotEmpty())
         Text(
             text = "Hello $name!",
             modifier = modifier
