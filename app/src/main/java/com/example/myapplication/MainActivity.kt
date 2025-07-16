dfef0e5ff6f6534180c4a525268a5329b204c68b
Megha Tavrech <megha.tavrech@galaxyweblinks.in>
2025-07-16 16:17:52 +0530

    test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,3 @@
-package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,8 +9,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import com.example.myapplication.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +25,11 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-<<<<<<< HEAD
                     greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                    
-=======
->>>>>>> b5e477780d5383aaa2502d84cba3059616e53576
+                    ShowImage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -39,11 +37,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+fun ShowImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "App Icon",
+        modifier = modifier
+    )
 }
 
-// test space
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
